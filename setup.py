3b650e90ccdbe6b6c97e0f470d7c462f21e77dd0
Jhonattan Jimenez <j1admin@noc-server.k12.vi>
2026-06-17 00:32:40 -0400

    fix: rename package metadata to j1-netvault

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='netvault-noc',
+    name='j1-netvault',
     version='1.0.0',
     packages=find_packages(),
     python_requires='>=3.8',
